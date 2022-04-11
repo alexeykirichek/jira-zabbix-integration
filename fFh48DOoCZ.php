823638d59bf97f14dedb824860eef33d23caa0f6
alexeykirichek <95589359+alexeykirichek@users.noreply.github.com>
2022-04-11 18:07:24 +0300

    Update fFh48DOoCZ.php

--- fFh48DOoCZ.php
+++ fFh48DOoCZ.php
@@ -5,11 +5,6 @@ $result_mysql_warning_info = $mysql_warning_info->query('SELECT COUNT(warning) F
 $result_mysql_warning_info = $result_mysql_warning_info->fetch_assoc(); // Сохраняем результат
 if (isset($result_mysql_warning_info)) {
     echo ($result_mysql_warning_info['COUNT(warning)']); // Выводим количество, если нашлось
-//    $result_mysql_warning_info_names = $mysql_warning_info->query('SELECT user FROM jira_issue WHERE warning=1'); // Запрашиваю имена авторов запросов
-//    for ($warning_info_names = array(); $row = $result_mysql_warning_info_names->fetch_assoc(); $warning_info_names[] = $row) ; //Сохраняю результат в массив
-//    foreach ($warning_info_names as $value) {
-//        echo "'".$value['user']."' ";
-//    }
 }
 else {
     echo 0; // Иначе выводим 0
